d2ad8578583633babaf8b8959971a5cfb6817493
SethPain <spm2667@louisiana.edu>
2017-10-18 00:25:02 -0500

    Moving Platforms
    Im currently trying to get the moving platforms working.

---
+++ Project_3/Assets/Scripts/PositionChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionChange : MonoBehaviour {
+
+    private bool keepGoing = true;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x > 150 && keepGoing)
+        {
+            transform.position += Vector3.left * .02f;
+        } else if (transform.position.x == 150 && keepGoing)
+        {
+            keepGoing = false;
+        } else if (transform.position.x < 166)
+        {
+            transform.position += Vector3.right * .02f;
+        } else if (transform.position.x == 166)
+        {
+            keepGoing = true;
+        }
+    }
+}
